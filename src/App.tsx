9921cc7fe42bf736a273ae51755f05c65c24a2d8
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2025-04-25 21:17:26 +0800

    feat: add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Navbar } from '@/components/Navbar';
+import { Hero } from '@/components/Hero';
+import { Highlights } from '@/components/Highlights';
+
 const App = () => {
-    return <div>hello world</div>;
+    return (
+        <main className="bg-black">
+            <Navbar />
+            <Hero />
+            <Highlights />
+        </main>
+    );
 };
 
 export default App;
